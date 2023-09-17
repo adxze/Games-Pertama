15e1442c1ec91502f5ebafdad1e827e48b65d2fe
adxze <adigunasanjaya2005@gmail.com>
2023-09-18 01:19:40 +0700

    im fucking tierd
    yes

--- gamemedival/Assets/Player_Script.cs
+++ gamemedival/Assets/Player_Script.cs
@@ -7,7 +7,7 @@ public class Player_Script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("hello word !");
+        Debug.Log("hello word ! !");
     }
 
     // Update is called once per frame
